d8c7a8db24ed848aa05124179bf11a8d522d4c81
preet81027@gmail.com <your-email@example.com>
2025-09-04 15:16:23 +0530

    remove hardcoded port

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bind to the port that Render provides via the PORT environment variable
-bind = f"0.0.0.0:{os.environ.get('PORT', 8000)}"
+bind = f"0.0.0.0:{os.environ.get('PORT')}"
 
 # Worker configuration  
 workers = 1
